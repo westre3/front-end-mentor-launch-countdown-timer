aa7cb899f35508c451127af7631085e8f6f0332a
Robert West <robert.trey.west@gmail.com>
2022-01-26 16:46:37 -0500

    Modularized code to support multiple cards

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-const card = document.querySelector('.card');
-let cardTop = document.querySelector('.card-top-position');
-let cardTopText = document.querySelector('.card-top-position p');
-let cardBottom = document.querySelector('.card-bottom-position');
-let cardBottomText = document.querySelector('.card-bottom-position p');
+animateCardFlip = (cardSelector, flipDuration, newValue) => {
+  let card = document.querySelector(cardSelector);
+  let cardTop = document.querySelector(cardSelector + ' .card-top-position');
+  let cardTopText = document.querySelector(cardSelector + ' .card-top-position p');
+  let cardBottom = document.querySelector(cardSelector + ' .card-bottom-position');
+  let cardBottomText = document.querySelector(cardSelector + ' .card-bottom-position p');
 
-const flipDuration = 500;
-const flipDelay = 500;
-document.documentElement.style.setProperty('--flip-duration', `${flipDuration}ms`);
-
-animateCardFlip = () => {
   // Create new card top
   let newCardTop = document.createElement('div');
-  newCardTop.className = 'card-half new-card-top-position card-top-color';
+  newCardTop.className = 'card-half new-card-top-position card-top-style';
   let newCardTopText = document.createElement('p');
-  newCardTopText.innerHTML = String(Number(cardTopText.innerHTML) - 1);
+  newCardTopText.innerHTML = newValue;
   newCardTop.appendChild(newCardTopText);
   card.appendChild(newCardTop);
 
   // animate flip
-  setTimeout(() => cardTop.classList.add('flip'), 0);
+  cardTop.style.animation = `flip forwards ${flipDuration}ms linear`;
 
   // at halfway point, change cardTopText since now it will appear
   // on the card's bottom
   setTimeout(() => {
-    cardTop.classList.remove('card-top-color');
-    cardTop.classList.add('card-bottom-color');
-    cardTopText.innerHTML = newCardTopText.innerHTML;
+    cardTop.classList.add('card-bottom-style');
+    cardTop.classList.remove('card-top-style');
+    cardTopText.innerHTML = newValue;
     cardTopText.style.transform = 'translateY(50%) rotateX(180deg)';
   }, flipDuration / 2);
 
@@ -34,30 +30,28 @@ animateCardFlip = () => {
   setTimeout(() => {
     card.removeChild(cardBottom);
 
-    cardBottom = cardTop;
-    cardBottomText = cardTopText;
-    cardBottom.classList.remove('flip');
-    cardBottom.classList.add('card-bottom-position');
-    cardBottom.classList.remove('card-top-position');
+    cardTop.style.animation = '';
+    cardTop.classList.remove('card-top-position');
+    cardTop.classList.add('card-bottom-position');
     cardTopText.style.transform = '';
 
-    cardTop = newCardTop;
-    cardTopText = newCardTopText;
-    cardTop.classList.remove('new-card-top-position');
-    cardTop.classList.add('card-top-position');
+    newCardTop.classList.remove('new-card-top-position');
+    newCardTop.classList.add('card-top-position');
   }, flipDuration);
 };
 
-const hide = (element) => {
-  element.classList.remove('visible');
-  element.classList.add('hidden');
-};
+let flipDuration = 500;
+let flipDelay = 1000;
 
-const show = (element) => {
-  element.classList.remove('hidden');
-  element.classList.add('visible');
-};
+document.documentElement.style.setProperty('--flip-duration', `${flipDuration}ms`);
 
-// for (let i = 0; i < 60; i++) {
-//   setTimeout(animateCardFlip, i * (flipDuration + flipDelay));
-// }
+for (let i = 0; i < 120; i++) {
+  setTimeout(animateCardFlip, i * flipDelay, '.card-1', flipDuration, String((120 - i - 1) % 60));
+}
+
+flipDuration = 500;
+flipDelay = 60 * 1000;
+
+for (let i = 0; i < 60; i++) {
+  setTimeout(animateCardFlip, i * flipDelay, '.card-2', flipDuration, String(60 - i - 1));
+}
