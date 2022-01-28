87699ea7e4ff00f6b6c3f9465e26b5022a30f971
Robert West <robert.trey.west@gmail.com>
2022-01-28 15:06:17 -0500

    Completed Layout

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-animateCardFlip = (cardSelector, flipDuration, newValue) => {
+const animateCardFlip = (cardSelector, flipDuration, newValue) => {
   let card = document.querySelector(cardSelector);
   let cardTop = document.querySelector(cardSelector + ' .card-top-position');
   let cardTopText = document.querySelector(cardSelector + ' .card-top-position p');
@@ -7,7 +7,7 @@ animateCardFlip = (cardSelector, flipDuration, newValue) => {
 
   // Create new card top
   let newCardTop = document.createElement('div');
-  newCardTop.className = 'card-half new-card-top-position card-top-style top-borders';
+  newCardTop.className = 'card-half new-card-top-position card-top-style card-top-borders';
   let newCardTopText = document.createElement('p');
   newCardTopText.innerHTML = newValue;
   newCardTop.appendChild(newCardTopText);
@@ -31,27 +31,62 @@ animateCardFlip = (cardSelector, flipDuration, newValue) => {
     card.removeChild(cardBottom);
     cardTop.style.animation = '';
     cardTop.classList.remove('card-top-position');
-    cardTop.classList.remove('top-borders');
+    cardTop.classList.remove('card-top-borders');
     cardTop.classList.add('card-bottom-position');
-    cardTop.classList.add('bottom-borders');
+    cardTop.classList.add('card-bottom-borders');
     cardTopText.style.transform = '';
     newCardTop.classList.remove('new-card-top-position');
     newCardTop.classList.add('card-top-position');
   }, flipDuration);
 };
 
-let flipDuration = 500;
-let flipDelay = 1000;
+const secondsPerMinute = 60;
+const secondsPerHour = secondsPerMinute * 60;
+const secondsPerDay = secondsPerHour * 24;
 
-document.documentElement.style.setProperty('--flip-duration', `${flipDuration}ms`);
+let countDownSeconds = 9 * secondsPerDay;
+const flipDelay = 1000;
+const flipDuration = 500;
 
-// for (let i = 0; i < 120; i++) {
-//   setTimeout(animateCardFlip, i * flipDelay, '.card-1', flipDuration, String((120 - i - 1) % 60).padStart(2, '0'));
-// }
+let daysDisplay = 9;
+let hoursDisplay = 0;
+let minutesDisplay = 0;
+let secondsDisplay = 0;
 
-// flipDuration = 500;
-// flipDelay = 60 * 1000;
+const intervalId = setInterval(() => {
+  countDownSeconds--;
+  if (countDownSeconds >= 0) {
+    let secondsLeft = countDownSeconds;
+    const days = Math.floor(countDownSeconds / secondsPerDay);
+    secondsLeft -= days * secondsPerDay;
 
-// for (let i = 0; i < 60; i++) {
-//   setTimeout(animateCardFlip, i * flipDelay, '.card-2', flipDuration, String(60 - i - 1).padStart(2, '0'));
-// }
+    const hours = Math.floor(secondsLeft / secondsPerHour);
+    secondsLeft -= hours * secondsPerHour;
+
+    const minutes = Math.floor(secondsLeft / secondsPerMinute);
+    secondsLeft -= minutes * secondsPerMinute;
+
+    const seconds = secondsLeft;
+
+    if (days !== daysDisplay) {
+      daysDisplay = days;
+      animateCardFlip('.days-card', flipDuration, String(daysDisplay).padStart(2, '0'));
+    }
+    if (hours != hoursDisplay) {
+      hoursDisplay = hours;
+      animateCardFlip('.hours-card', flipDuration, String(hoursDisplay).padStart(2, '0'));
+    }
+    if (minutes != minutesDisplay) {
+      minutesDisplay = minutes;
+      animateCardFlip('.minutes-card', flipDuration, String(minutesDisplay).padStart(2, '0'));
+    }
+    if (seconds != secondsDisplay) {
+      secondsDisplay = seconds;
+      animateCardFlip('.seconds-card', flipDuration, String(secondsDisplay).padStart(2, '0'));
+    }
+  }
+
+  if (countDownSeconds === 0) {
+    clearInterval(intervalId);
+  }
+}, flipDelay);
